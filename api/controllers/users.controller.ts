39b738c8558a78556dd39fc1c1817b550ab97817
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2022-06-04 22:09:58 +0200

    change files

--- api/controllers/users.controller.ts
+++ api/controllers/users.controller.ts
@@ -2,15 +2,13 @@
 import * as crud from "../services/filmen.crud.ts";
 // @ts-ignore
 import { filmenConnect } from "../services/connection.ts";
+import { Request, Function } from "express";
 
 const Filmen = filmenConnect();
 
 export const getUsers = async (req: Request, res: Request, next: Function) => {
     try {
-        crud.getUsers(Filmen).then((data: any) => {
-            res.json();
-            console.log(data);
-        });
+        crud.getUsers(Filmen).then((data: any) => res.json(data));
     } catch (err) {
         next(err);
     }
